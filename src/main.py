da213ffb7de19945921e4976a368ebd8580685da
james <james.r.menzies@gmail.com>
2020-09-11 13:58:01 +1000

    basic loop created, synonyms property added

--- src/main.py
+++ src/main.py
@@ -1,3 +1,16 @@
-import display_
+import display_utils
+from word import Word
 
-options = 
+def word_lookup():
+    user_input = input("Please enter your word >> ")
+    word = Word.get_word_from_string(user_input)
+
+    
+
+
+options, items = display_utils.create_option_block()
+items["Look up a word"] = word_lookup
+
+display_utils.choice_loop(options)
+
+
--- src/word.py
+++ src/word.py
@@ -8,3 +8,12 @@ class Word:
         word.name = 
 
         return word
+
+    @property
+    def synonyms(self):
+        if self._synonyms:
+            return self.synonyms
+        else:
+            # make API call
+            # save response into self._synonyms
+            # return self._synonyms
